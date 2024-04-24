7d88f2f03d0b0059adebf5d6d6d442c821c64be2
mirzausman <mirzausman9006@gmail.com>
2024-04-24 15:00:09 +0500

    changes for correct the error

--- app/press-dashboard-admin/page.jsx
+++ app/press-dashboard-admin/page.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import AdminPanel from "../components/AdminPanel";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
@@ -7,15 +7,24 @@ import { useRouter } from "next/navigation";
 const Page = () => {
   const { data: session, status: sessionStatus } = useSession();
   const router = useRouter();
+  const [refreshed, setRefreshed] = useState(false);
+
   useEffect(() => {
     if (session?.user?.role === "user") {
       router.replace("/press-dashboard/pr-balance");
     } else if (sessionStatus === "unauthenticated") {
       router.replace("/login");
-    }else{
-      window.location.reload();
     }
   }, [sessionStatus, router]);
+
+  useEffect(() => {
+    // Only refresh the page when it mounts for the first time
+    if (!refreshed) {
+      window.location.reload();
+      setRefreshed(true); 
+    }
+  }, [refreshed]);
+
   return (
     <div>
       <AdminPanel />
