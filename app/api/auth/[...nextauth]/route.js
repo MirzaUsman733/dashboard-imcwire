f3b39b008999c74bcc152cda92b5f9b2876afef9
mirzausman <mirzausman9006@gmail.com>
2024-04-26 17:17:56 +0500

    changes

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -120,7 +120,8 @@ const authOptions = {
 async function verifyRecaptcha(token) {
     const secretKey = process.env.RECAPTCHA_SECRET_KEY;
     const response = await fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${token}`, {
-      method: "POST"
+      method: "POST",
+      body: JSON.stringify({ token }),
     });
     const data = await response.json();
     return data.success;
