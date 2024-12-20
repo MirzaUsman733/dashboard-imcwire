8a5542b16dbd69fb720935a310f94adb2393eb47
muhammad usman <hafizusman733k@gmail.com>
2024-12-20 19:21:05 +0500

    .............

--- app/layout.js
+++ app/layout.js
@@ -20,11 +20,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script 
+          dangerouslySetInnerHTML={{ 
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "pgxhnlvp0v");
+            ` 
+          }} 
+        />
+      </head>
       <body className={inter.className}>
-        {/* <script
-          crossOrigin="anonymous"
-          src="https://www.google.com/recaptcha/api.js"
-        ></script> */}
         <SessionProvider>
           <UserProvider>
             <DataProvider>
@@ -35,7 +44,6 @@ export default function RootLayout({ children }) {
                       {children}
                     </SessionTimeout>
                   </ToastProvider>
-                  {/* <ToastContainer /> */}
                 </DistributionProvider>
               </UpdateDataProvider>
             </DataProvider>
